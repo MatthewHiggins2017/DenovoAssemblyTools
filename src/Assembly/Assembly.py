b0c1d41700360e9aed3f29870d77a3159eb67667
MatthewHiggins2017 <matthew.higgins2017@gmail.com>
2024-05-08 13:16:21 +0100

    Using python to find the nextflow scripts

--- src/Assembly/Assembly.py
+++ src/Assembly/Assembly.py
@@ -1,9 +1,22 @@
+import os
+
 def add_numbers(num1, num2):
     return num1 + num2
 
 def main():
     result = add_numbers(3, 5)
     print(f"The result is {result}")
+
+    # Get the absolute path of the currently running script
+    script_path = os.path.abspath(__file__)
+    print("Script path:", script_path)
+
+    # Get the directory path of the currently running script.
+    # This is how we can point to nextflow scripts . 
+    nextflow_directory_path = os.path.dirname(os.path.dirname(os.path.dirname(script_path))) + '/NF/'
+    print("Parent Directory:",nextflow_directory_path)
+
+
 
 if __name__ == "__main__":
     main()

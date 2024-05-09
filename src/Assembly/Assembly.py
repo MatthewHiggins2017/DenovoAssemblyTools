5eaf10983d3428d9f8c0638f1866fbd045d050a0
MatthewHiggins2017 <matthew.higgins2017@gmail.com>
2024-05-09 13:33:48 +0100

    Adding parameters to python script

--- src/Assembly/Assembly.py
+++ src/Assembly/Assembly.py
@@ -1,20 +1,150 @@
-import os
+#!/usr/bin/env python3
 
-def add_numbers(num1, num2):
-    return num1 + num2
+import os
+import sys
+import argparse
+
+def FullAssembly(args):
+    print('Hello World')
+    return 
+
+
+def AssemblyAssess(args):
+    print('Hello World')
+    return 
+
+
 
 def main():
-    result = add_numbers(3, 5)
-    print(f"The result is {result}")
+    print("DeNovo Assembly Assessment")
 
-    # Get the absolute path of the currently running script
-    script_path = os.path.abspath(__file__)
-    print("Script path:", script_path)
+    # Obtain Package Path
+    PackagePath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
 
-    # Get the directory path of the currently running script.
-    # This is how we can point to nextflow scripts . 
-    nextflow_directory_path = os.path.dirname(os.path.dirname(os.path.dirname(script_path))) + '/NF/'
-    print("Parent Directory:",nextflow_directory_path)
+    print("Package Location:",PackagePath)
+
+
+    ###############################
+    # Parse Command Line Arguments
+    ###############################
+    parser = argparse.ArgumentParser(description='DeNovo Assembly')
+
+
+    # Subparser to assess an assembly
+    subparsers = parser.add_subparsers(help="Options")
+
+
+
+
+    ###############################
+    # Sub Parser One - Assembly
+    ###############################
+    parser_sub_one = subparsers.add_parser('Assemble',
+                                        help='Full Assembly')
+
+    parser_sub_one.add_argument('--PackagePath',
+                                help='Path to the package directory so all nextflow scripts and singularity containers are accessible.',
+                                type=str,
+                                default=PackagePath)
+
+
+    parser_sub_one.add_argument('--ID',
+                                help='',
+                                type=str,
+                                required=True)
+    
+    
+    parser_sub_one.add_argument('--Outdir',
+                                    help='',
+                                    type=str,
+                                    required=True)
+
+    parser_sub_one.add_argument('--Fastq',
+                                help='',
+                                type=str)
+
+    parser_sub_one.add_argument('--Threads',
+                                    help='',
+                                    type=int,
+                                    default=2)
+    
+    parser_sub_one.add_argument('--KrakenReport',
+                                help='',
+                                type=str)
+    
+    parser_sub_one.add_argument('--KrakenOutput',
+                                help='',
+                                type=str)
+
+    parser_sub_one.add_argument('--ExcludeTaxID',
+                                help='',
+                                type=str)
+    
+
+    parser_sub_one.add_argument('--MinLength',
+                                help='',
+                                default=1000,
+                                type=int)
+    
+
+    parser_sub_one.add_argument('--MinQuality',
+                                help='',
+                                default=7,
+                                type=int)
+    
+
+    parser_sub_one.add_argument('--ReadError',
+                                help='',
+                                default=0.06,
+                                type=float)
+    
+    
+    parser_sub_one.add_argument('--NtLinkRounds',
+                                help='',
+                                default=0.06,
+                                type=int)
+    
+    parser_sub_one.add_argument('--GuideReference',
+                                help='',
+                                type=str)
+    
+
+    parser_sub_one.set_defaults(func=FullAssembly)
+
+
+
+    ###############################
+    # Sub Parser Two - Assess
+    ###############################
+    parser_sub_two = subparsers.add_parser('Assess',
+                                        help='Assessment of Assembly')
+
+    parser_sub_two.add_argument('--Prefix','-P',
+                            help='Prefix for all files generated',
+                            required=True)
+
+    parser_sub_two.set_defaults(func=AssemblyAssess)
+
+
+
+    ###############################
+    # No Arguments
+    ###############################
+
+    # If no options specified add help
+    if len(sys.argv) <= 1:
+        sys.argv.append('--help')
+
+
+
+    # Trigger parser specific arguments. 
+    args = parser.parse_args()
+    args.func(args)
+
+
+
+
+
 
 
 
